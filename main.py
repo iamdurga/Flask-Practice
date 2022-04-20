289683b0c5f64b7422dbaf779eea413d55728212
Durga Pokharel <pokhareldurga88@gmail.com>
2022-04-20 22:54:44 +0545

    minor changes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, make_response
 import pandas as pd
 
 app = Flask(__name__)
@@ -15,7 +15,11 @@ def user(name):
 def titanic():
     df = pd.read_csv("data/titanic.csv")
     return "<h1 align= 'center'> Below Table is Titanic Dataset</h1>" + df.to_html()
-
+@app.route("/cookie")
+def cookie():
+    res = make_response("Setting my cookie")
+    res.set_cookie("visits","1", max_age=60*60*24)
+    return res.get_data()
 
 if __name__== "__main__":
     app.run(debug=True)
